07a08449da6a121aeeab93e475f8d0d538e747c8
gaurav sharma <gaurav552sharma@example.com>
2024-06-15 12:56:59 +0530

    updated cors origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app=express();
 
 app.use(express.json({extended:true}));
 app.use(express.urlencoded({extended:true}));
-app.use(cors({extended:true,origin:"http://localhost:3000",credentials: true}));
+app.use(cors({extended:true,origin:"https://blog-app-frontend-beryl.vercel.app",credentials: true}));
 app.use(upload());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 
